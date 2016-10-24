42bdb604fb67f9a6b54a9352dabad2220b357d8d
Jimmy911009 <lan@lanoudeMac-mini-8.local>
2016-10-24 16:12:04 +0800

    修改轮播
    asdf

--- js/hear.js
+++ js/hear.js
@@ -7,27 +7,26 @@ var str = 1;
 //获取图片文字
 var imgCent	 = $("#slider_cent span");
 
+//获取图片宽度保存到数组
+var imgArr = ["sliderO","sliderA","sliderB","sliderC","sliderD"];
+
+
 function imgsFn(){
-
-		$sliderImg.css({"transform": "scale(0.7)"});
-		$slider.css({"transition": "1s"});
-
-		$slider.animate({"marginLeft":-str*100+"%"},1000,"linear",function(){
-			setTimeout(function(){
-				$sliderImg.css({"transform": "scale(1)"});
-				bol = true;			
-			},1000);
-			if(str > 4){
-				setTimeout(function(){
-					$slider.css({"marginLeft":"0%","transition": "0s"});		
-				},1500);
-				 str = 1;
-			}
-			console.log("还原图片");
-		});
-
-		str++;
-
+	console.log("却换");
+	$slider.css("transition","1s");
+	$slider.attr('class',imgArr[str]);
+	str++;
+	if(str == 5){
+		$slider.attr('class',imgArr[5]);
+		setTimeout(function(){
+			str =1;
+			$slider.css("transition","0s");
+			$slider.attr('class',imgArr[0]);
+			// bol = !bol;
+			console.log("完成却换");			
+		},1000);
+	}
+	bol = !bol;
 }
 
 //创建一个定时器累加时间
@@ -35,11 +34,14 @@ var cenNum = 0;
 //定义BOL值控制事件调取
 var bol = true;
 setInterval(function(){
+	console.log(str);
 	if(bol){
+
 		cenNum +=2;
 		// imgCent.eq(str-1).css("width",cenNum+"%").siblings().css("width","0%");
 		imgCent.each(function(i){
 			if(i == str-1){
+				console.log('a');
 				imgCent.eq(str-1).css("width",cenNum+"%");
 			}else{
 				imgCent.eq(i).css("width","0%");
@@ -48,7 +50,7 @@ setInterval(function(){
 		if(cenNum >= 100){
 			imgsFn();
 			cenNum = 0;
-			bol = false;
+			bol = !bol;
 		}		
 	}
 
